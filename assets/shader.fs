52c2d2010ca6610de8301a60f17046f0da600c13
Stachu1 <stach.basiuk@gmail.com>
2024-05-26 00:35:12 +0200

    Added specular reflections

--- assets/shader.fs
+++ assets/shader.fs
@@ -29,12 +29,16 @@ const vec3 bg_color = vec3(0.0, 0.2, 0.2);
 const Camera camera = Camera(vec3(0.0, 0.0, -1.6), vec3(0.0, 0.0, 1.0));
 const Sphere sphere = Sphere(vec3(0.0, 0.0, 0.0), 1.0, vec3(1.0, 0.8, 0.2));
 const float gamma = 1.0 / 1.6;
+const vec3 light_position_1 = vec3(-5.0, 5.0, -2.0);
+const vec3 light_color_1 = vec3(0.7, 0.0, 1.0);
+const vec3 light_position_2 = vec3(5.0, 5.0, -2.0);
+const vec3 light_color_2 = vec3(0.0, 0.7, 1.0);
 
 vec3 ray_march(Ray ray);
 vec3 get_normal(vec3 point);
 float map_the_world(vec3 point);
 float get_distance_from_sphere(vec3 point, vec3 center, float radius);
-vec3 get_color(vec3 ray_dir, vec3 point);
+vec3 get_color(vec3 ray_dir, vec3 norm, vec3 point);
 vec3 apply_gamma(vec3 color);
 
 
@@ -57,11 +61,24 @@ vec3 apply_gamma(vec3 color) {
     return vec3(pow(color.x, gamma), pow(color.y, gamma), pow(color.z, gamma));
 }
 
-vec3 get_color(vec3 ray_dir, vec3 point) {
-    vec3 norm = get_normal(point - sphere.center);
-    float b = dot(-norm, ray_dir);
-    vec3 c = sphere.color;
-    return vec3(c.x * b, c.y * pow(b, 3.0), c.z * (0.5-b*0.5));
+vec3 get_color(vec3 ray_dir, vec3 norm, vec3 point) {
+    float brightness = dot(norm, -ray_dir);
+    vec3 color = sphere.color;
+    color.x *= brightness;
+    color.y *= pow(brightness, 2.5);
+    color.z *= (0.5 - 0.5 * brightness);
+
+    vec3 view_dir = normalize(camera.position - point);
+
+    vec3 halfway_dir = normalize(normalize(light_position_1 - point) + view_dir);
+    float spec = pow(max(dot(norm, halfway_dir), 0.0), 32.0);
+    vec3 specular = light_color_1 * spec;
+
+    halfway_dir = normalize(normalize(light_position_2 - point) + view_dir);
+    spec = pow(max(dot(norm, halfway_dir), 0.0), 32.0);
+    specular += light_color_2 * spec;
+
+    return color + specular;
 }
 
 float get_distance_from_sphere(vec3 point, vec3 center, float radius) {
@@ -97,7 +114,10 @@ vec3 ray_march(in Ray ray) {
         float dis_to_closest = map_the_world(pos);
 
         if (dis_to_closest < MIN_HIT_DIS) {
-            return get_color(ray.direction, pos);
+            vec3 norm = get_normal(pos - sphere.center);
+            // vec3 dir_to_light = normalize(pos - light_position);
+            // float diffuse_intensity = max(0.0, dot(norm, dir_to_light));
+            return get_color(ray.direction, norm, pos);
         }
 
         if (dis_traveled > MAX_TRACE_DIS) {
